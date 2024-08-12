20cd04924e6e23b9e5cd0297363c44bb58d74f25
hoavnk <hoavnk@theplus1.net>
2024-08-12 10:43:02 +0700

    Update wp-config.php

--- freshlife247/wp-config.php
+++ freshlife247/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'freshlife247' );
+define( 'DB_NAME', 'your_db_name' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'your_db_user' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'your_db_password' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
